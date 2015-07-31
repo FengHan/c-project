b19178521a1ae51753f228fcc3485f862c54fd66
Michael <keke231@qq.com>
2015-07-31 16:35:59 +0800

    paixu

---
+++ first_c/ji_ou_pai_xu2.cc
@@ -0,0 +1,37 @@
+/*
+编程题＃3：奇偶排序（二）
+注意： 总时间限制: 1000ms 内存限制: 65536kB
+描述
+和上题一样，但是要求用第二种解法
+输入
+输入十个整数
+输出
+按照奇偶排序好的十个整数
+*/
+#include <iostream>
+using namespace std;
+int main () {
+	int a[10];
+	for (int i = 0; i< 10; i++) {
+		cin >> a[i];
+	}
+	//冒泡， 不断比较两个相邻的数，如果顺序错了，那么就交换
+	for (int i = 0; i < 9; i++) {
+		for (int j = 1; j < 10 - i; j++) {
+			//与刚才的冒泡排序法不同，我们不只是通过数字的大小决定顺序
+			//如果左边为偶数，右边为奇数，那么顺序也颠倒
+			bool leftIsEven = a[j - 1] % 2 == 0;
+			bool rightIsEven = a[j] % 2 == 0;
+			if ((leftIsEven && !rightIsEven)|| 
+				(leftIsEven == rightIsEven && a[j - 1] > a[j])) {
+				int temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << a[i] << ' ';
+	}
+	return 0;
+}
--- first_c/maopao.cc
+++ first_c/maopao.cc
@@ -1,5 +1,16 @@
+/*
+ 作业:
+ Programming Assignment: 感性接触计算机程序
+ 编程题＃1：实现冒泡排序
+ 请根据自己的理解编写冒泡排序算法，数组大小1000以内
+ 输入
+ 第一行是n，表示数组的大小
+ 接着n行是数组的n个元素
+ 输出
+ 排序之后的结果
+ 一个元素一行
+ */
 #include <iostream>
-
 using namespace std;
 int main() {
     int n, a[1000]; //一共n个数，n不超过1000。a用来保存这些数
@@ -10,7 +21,7 @@ int main() {
     }
     //冒泡，不断比较两个相邻的两个数，如果顺序错了，那么就交换
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 1; j < n - 1; j++) {
+        for (int j = 1; j < n - i; j++) {
             if (a[j - 1] > a[j]) {
                 int temp = a[j];
                 a[j] = a[j - 1];
@@ -18,6 +29,7 @@ int main() {
             }
         }
     }
+	
     //依次输出
     for (int i = 0; i < n; i++) {
         cout << a[i] << endl;
